4a3ad51597919bcbf1e8f057fe6d3c4a79e04472
hoanghnjob <87923705+hoanghnjob@users.noreply.github.com>
2023-07-05 09:01:24 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Quỳnh Anh à!',
+    introDesc: `Trái đất Con người luôn luôn sẽ thay đổi.
+    Lúc đầu tớ thích cậu, càng về sau tới lại càng thích cậu hơn.`,
+    btnIntro: 'Xem có gì nào!',
+    title: 'CHÚC MỪNG SANH THẦN BÉ IU CỤA TUI 😙',
+    desc: 'Yêu tui hong?',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Một người vừa đẹp trai, tài giỏi, lại còn thương người iêu thì phải yêu chớ!',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chúc em có một sinh nhật thật là vui vẻ, hạnh phúc và yêu anh nhiều hơn!!!',
+    btnAccept: 'Dạaaaa <3',
+    messLink: 'https://www.youtube.com/watch?v=S7KA4tQ483o' 
+}
